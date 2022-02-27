1ee52764cf9431fb922ed58703e49255697a2458
Dishant Nagpal <dishantnagpal10@gmail.com>
2022-02-27 22:24:14 +0530

    Initial Commit

--- Poke3D/ViewController.swift
+++ Poke3D/ViewController.swift
@@ -10,9 +10,10 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
     
+    // var pokeScene=SCNScene()
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,19 +23,24 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        sceneView.autoenablesDefaultLighting=true
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
+        let configuration = ARImageTrackingConfiguration()
+        
+        
+        if let imageToTrack=ARReferenceImage.referenceImages(inGroupNamed: "Pokemon Cards", bundle: Bundle.main){
+            configuration.trackingImages=imageToTrack
+            configuration.maximumNumberOfTrackedImages=2
+            print("images successfully added")
+            
+        }
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -45,30 +51,44 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
+    
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
+        let node=SCNNode()
+        
+        if let imageAnchor=anchor as? ARImageAnchor{
+            let cardName=imageAnchor.referenceImage.name
+            let plane=SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+            plane.firstMaterial?.diffuse.contents=UIColor(white: 1.0, alpha: 0.5)
+            let planeNode=SCNNode(geometry: plane)
+            planeNode.eulerAngles.x = -Float.pi/2
+            node.addChildNode(planeNode)
+            DispatchQueue.main.async {
+                if cardName=="evee"{
+                    
+                    if let pokeScene=SCNScene(named: "art.scnassets/eevee.scn"){
+                        print("yes1")
+                        if let pokeNode=pokeScene.rootNode.childNode(withName: "eevee", recursively: true){
+                            print("yes2")
+                            pokeNode.eulerAngles.x = Float.pi/2
+                            planeNode.addChildNode(pokeNode)
+                        }
+                    }
+                }
+                else if cardName=="oddish"{
+                    
+                    if let pokeScene=SCNScene(named: "art.scnassets/oddish.scn"){
+                        print("yes1")
+                        if let pokeNode=pokeScene.rootNode.childNode(withName: "oddish", recursively: true){
+                            print("yes2")
+                            pokeNode.eulerAngles.x = Float.pi/2
+                            planeNode.addChildNode(pokeNode)
+                        }
+                    }
+                }
+            }
+            
+        }
         return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
     }
 }
